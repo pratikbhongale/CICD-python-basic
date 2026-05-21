38a523d18193bf65800f374b306bfa6762ad64e4
pratikbhongale <pratikbhongale01@gmail.com>
2026-05-21 15:27:49 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,3 @@
-# from flask import Flask
-# from calculator import add
-# app = Flask(__name__)
-# @app.route("/")
-# def home():
-#     return f"2 + 3 = {add(2,3)}"
-# if __name__ == "__main__":
-#     app.run(hosts="0.0.0.0", port=5000)
-#above version is not compatible with flake8
-#it expectes space below and above functions
 from flask import Flask
 from calculator import add
 
