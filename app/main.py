f9dbed1ad7aceb1afa0d6690e01046524d232405
pratikbhongale <pratikbhongale01@gmail.com>
2026-05-21 10:54:15 +0530

    source code

---
+++ app/main.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from calculator import add
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return f"2 + 3 = {add(2,3)}"
+if __name__ == "__main__":
+    app.run(hosts="0.0.0.0", port=5000)
